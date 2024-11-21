16795bc2c92ee88cf0afa35f8373a0efde0087da
oscarprdev <vlcoscar8@gmail.com>
2024-11-21 16:56:26 +0100

    Add seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
+import vercel from '@astrojs/vercel/serverless';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
 	compressHTML: true,
+	output: 'server',
+	adapter: vercel(),
 });
